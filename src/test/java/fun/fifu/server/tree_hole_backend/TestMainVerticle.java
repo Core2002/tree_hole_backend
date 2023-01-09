9d379330508825711fd5458e273a901f665a995d
NekokeCore <Core2002@163.com>
2023-01-10 04:43:02 +0800

    add test method savePojo.

--- src/test/java/fun/fifu/server/tree_hole_backend/TestMainVerticle.java
+++ src/test/java/fun/fifu/server/tree_hole_backend/TestMainVerticle.java
@@ -1,22 +1,35 @@
 package fun.fifu.server.tree_hole_backend;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
-
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
-  }
+  Vertx vertx = Vertx.vertx();
 
   @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    testContext.completeNow();
+  void savePojo(VertxTestContext testContext) {
+    MongoClient client = MongoClient.createShared(vertx, new JsonObject()
+      .put("host", "localhost")
+      .put("port", 27017)
+      .put("db_name", "tree_hole")
+    );
+
+    JsonObject document = new JsonObject().put("name", "Alan Turing");
+    client.save("test", document).onComplete(handler -> {
+      if (handler.succeeded()) {
+        System.out.println(handler.result());
+        testContext.completeNow();
+      } else {
+        testContext.failNow("F - " + handler.result());
+      }
+    });
+
   }
+
 }
